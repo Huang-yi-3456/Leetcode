3f4268f1c8123b4aad1dd5d524f3069f9ce35c02
Huang-Yi <huang.yi.3456@gmail.com>
2020-05-09 19:20:23 +0200

    easy

---
+++ src/leetcode/editor/en/[167]Two Sum II - Input array is sorted.java
@@ -0,0 +1,42 @@
+//Given an array of integers that is already sorted in ascending order, find two
+// numbers such that they add up to a specific target number. 
+//
+// The function twoSum should return indices of the two numbers such that they a
+//dd up to the target, where index1 must be less than index2. 
+//
+// Note: 
+//
+// 
+// Your returned answers (both index1 and index2) are not zero-based. 
+// You may assume that each input would have exactly one solution and you may no
+//t use the same element twice. 
+// 
+//
+// Example: 
+//
+// 
+//Input: numbers = [2,7,11,15], target = 9
+//Output: [1,2]
+//Explanation: The sum of 2 and 7 is 9. Therefore index1 = 1, index2 = 2. 
+// Related Topics Array Two Pointers Binary Search
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int start = 0;
+        int end = numbers.length - 1;
+        while (start < end) {
+            int sum = numbers[start] + numbers[end];
+            if (sum == target) {
+                return new int[] {start+1, end+1};
+            } else if (sum < target) {
+                start++;
+            } else {
+                end--;
+            }
+        }
+        return new int[] {-1, -1};
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
