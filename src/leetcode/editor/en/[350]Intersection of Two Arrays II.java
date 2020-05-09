fb7a165de816ce919ec4221f1d56734fe27b38b5
Huang-Yi <huang.yi.3456@gmail.com>
2020-05-09 19:13:51 +0200

    when both are sorted

--- src/leetcode/editor/en/[350]Intersection of Two Arrays II.java
+++ src/leetcode/editor/en/[350]Intersection of Two Arrays II.java
@@ -39,19 +39,21 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        if (nums1.length > nums2.length) {
-            return intersect(nums2, nums1);
-        }
-        Map<Integer, Integer> m1 = new HashMap<>();
-        for (int num : nums1) {
-            m1.put(num, m1.getOrDefault(num, 0) + 1);
-        }
-        int k = 0;
-        for (int num : nums2) {
-            int count = m1.getOrDefault(num, 0);
-            if (count > 0) {
-                nums1[k++] = num;
-                m1.put(num, m1.get(num) - 1);
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+
+        int i = 0, j = 0, k = 0;
+        while(i < nums1.length && j < nums2.length) {
+            if (nums1[i] == nums2[j]) {
+                nums1[k++] = nums2[j];
+                i++;
+                j++;
+                continue;
+            }
+            if (nums1[i] < nums2[j]) {
+                i++;
+            } else {
+                j++;
             }
         }
         return Arrays.copyOfRange(nums1, 0, k);
