044deb0d00574aee56d7c5f5186a61b33585334e
Huang-Yi <huang.yi.3456@gmail.com>
2020-05-12 23:16:33 +0200

    Iterative

---
+++ src/leetcode/editor/en/[94]Binary Tree Inorder Traversal.java
@@ -0,0 +1,51 @@
+//Given a binary tree, return the inorder traversal of its nodes' values. 
+//
+// Example: 
+//
+// 
+//Input: [1,null,2,3]
+//   1
+//    \
+//     2
+//    /
+//   3
+//
+//Output: [1,3,2] 
+//
+// Follow up: Recursive solution is trivial, could you do it iteratively? 
+// Related Topics Hash Table Stack Tree
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> ret = new LinkedList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        while (!stack.isEmpty() || root != null) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            TreeNode leftMost = stack.pop();
+            ret.add(leftMost.val);
+            root = leftMost.right;
+        }
+        return ret;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
