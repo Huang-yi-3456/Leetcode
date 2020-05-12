3207aa23bcb5600c1513d2722b44fdf416ab7788
Huang-Yi <huang.yi.3456@gmail.com>
2020-05-12 22:57:26 +0200

    iterative

--- src/leetcode/editor/en/[100]Same Tree.java
+++ src/leetcode/editor/en/[100]Same Tree.java
@@ -60,10 +60,42 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (!check(p, q)) return false;
+        Stack<TreeNode> s1 = new Stack<>();
+        if (p != null)
+            s1.push(p);
+        Stack<TreeNode> s2 = new Stack<>();
+        if (q != null)
+            s2.push(q);
+
+        while(!s1.isEmpty()) {
+            if (s2.isEmpty()) return false;
+            TreeNode c1 = s1.pop();
+            TreeNode c2 = s2.pop();
+            if (!check(c1.right, c2.right)) return false;
+            if (c1.right != null) {
+
+                s1.push(c1.right);
+                s2.push(c2.right);
+            }
+            if (!check(c1.left, c2.left)) return false;
+            if (c1.left != null) {
+                s1.push(c1.left);
+                s2.push(c2.left);
+            }
+
+        }
+        if (!s2.isEmpty()) return false;
+        return true;
+    }
+
+    private boolean check(TreeNode p, TreeNode q) {
+        // p and q are null
         if (p == null && q == null) return true;
-        if (p == null || q == null) return false;
+        // one of p and q is null
+        if (q == null || p == null) return false;
         if (p.val != q.val) return false;
-        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        return true;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
