e8055d5b55e70d763c0647aaf129bc86189c92a1
Huang-Yi <huang.yi.3456@gmail.com>
2020-05-15 12:53:28 +0200

    reverse

---
+++ src/leetcode/editor/en/[234]Palindrome Linked List.java
@@ -0,0 +1,58 @@
+//Given a singly linked list, determine if it is a palindrome. 
+//
+// Example 1: 
+//
+// 
+//Input: 1->2
+//Output: false 
+//
+// Example 2: 
+//
+// 
+//Input: 1->2->2->1
+//Output: true 
+//
+// Follow up: 
+//Could you do it in O(n) time and O(1) space? 
+// Related Topics Linked List Two Pointers
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) return true;
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        ListNode end = slow;
+        ListNode pre = slow;
+        ListNode cur = slow.next;
+        ListNode next = null;
+        while (cur != null) {
+            next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        while (head != end.next && pre != end) {
+            if (head.val != pre.val) return false;
+            head = head.next;
+            pre = pre.next;
+        }
+        return true;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
