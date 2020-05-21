0160b61a1cf84ef200bc63a1b70b0637ee696c1a
Huang-Yi <huang.yi.3456@gmail.com>
2020-05-21 12:59:23 +0200

    easy

---
+++ src/leetcode/editor/en/[1]Two Sum.java
@@ -0,0 +1,33 @@
+//Given an array of integers, return indices of the two numbers such that they a
+//dd up to a specific target. 
+//
+// You may assume that each input would have exactly one solution, and you may n
+//ot use the same element twice. 
+//
+// Example: 
+//
+// 
+//Given nums = [2, 7, 11, 15], target = 9,
+//
+//Because nums[0] + nums[1] = 2 + 7 = 9,
+//return [0, 1].
+// 
+// Related Topics Array Hash Table
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] ret = new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; ++i) {
+            if (map.containsKey(nums[i])) {
+                return new int[]{map.get(nums[i]), i};
+            } else {
+                map.put(target - nums[i], i);
+            }
+        }
+        return ret;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
