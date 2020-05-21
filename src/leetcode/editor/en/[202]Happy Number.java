deed1b97267dbebd672419fffdf8432681ac96e0
Huang-Yi <huang.yi.3456@gmail.com>
2020-05-21 12:58:56 +0200

    easy

---
+++ src/leetcode/editor/en/[202]Happy Number.java
@@ -0,0 +1,48 @@
+//Write an algorithm to determine if a number n is "happy". 
+//
+// A happy number is a number defined by the following process: Starting with an
+//y positive integer, replace the number by the sum of the squares of its digits, 
+//and repeat the process until the number equals 1 (where it will stay), or it loo
+//ps endlessly in a cycle which does not include 1. Those numbers for which this p
+//rocess ends in 1 are happy numbers. 
+//
+// Return True if n is a happy number, and False if not. 
+//
+// Example: 
+//
+// 
+//Input: 19
+//Output: true
+//Explanation: 
+//12 + 92 = 82
+//82 + 22 = 68
+//62 + 82 = 100
+//12 + 02 + 02 = 1
+// 
+// Related Topics Hash Table Math
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public boolean isHappy(int n) {
+        Set<Integer> seen = new HashSet<>();
+        while (n != 1) {
+            if (seen.contains(n)) return false;
+            seen.add(n);
+            n = squDigt(n);
+
+        }
+        return true;
+    }
+
+    int squDigt(int n) {
+        int sum = 0;
+        while (n != 0) {
+            int last = n % 10;
+            sum += last * last;
+            n /= 10;
+        }
+        return sum;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
